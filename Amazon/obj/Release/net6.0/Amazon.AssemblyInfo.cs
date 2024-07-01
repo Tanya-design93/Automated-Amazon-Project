c966ed5fbceff7e01012ab6838446e750b07b71d
Tanya-design93 <67875354+Tanya-design93@users.noreply.github.com>
2024-07-01 17:17:59 -0300

    init

--- Amazon/obj/Release/net6.0/Amazon.AssemblyInfo.cs
+++ Amazon/obj/Release/net6.0/Amazon.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Amazon")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d5c20a8a52fbfb03908dd64c0a61139fa0eb65")]
 [assembly: System.Reflection.AssemblyProductAttribute("Amazon")]
